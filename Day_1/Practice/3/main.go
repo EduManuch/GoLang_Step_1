af1ed46fb918a1041ade2b9d2acff31669ea8db5
Eduard <test@mail.ru>
2024-11-30 16:56:39 +0300

    OK

---
+++ Day_1/Practice/3/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var (
+		sum   float64
+		years int64
+		rate  int64
+	)
+
+	fmt.Println("Enter sum:")
+	fmt.Scan(&sum)
+	fmt.Println("Enter years:")
+	fmt.Scan(&years)
+	fmt.Println("Enter rate:")
+	fmt.Scan(&rate)
+
+	if sum < 100 || sum > 1000000 {
+		fmt.Println("Incorrect sum")
+	}
+	if years < 1 || years > 100 {
+		fmt.Println("Incorrect years")
+	}
+	if rate < 1 || rate > 20 {
+		fmt.Println("Incorrect rate")
+	}
+
+	result := sum * math.Pow(1+float64(rate)/100, float64(years))
+	fmt.Printf("Total result is %.2f\n", result)
+}
