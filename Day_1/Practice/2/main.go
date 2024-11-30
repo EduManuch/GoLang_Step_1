5ac7718d9ec2fdf07fad48915b371a09637a4d22
Eduard <test@mail.ru>
2024-11-30 15:23:32 +0300

    OK

--- Day_1/Practice/2/main.go
+++ Day_1/Practice/2/main.go
@@ -6,7 +6,7 @@ func main() {
 	var n uint8
 	fmt.Scan(&n)
 
-	fmt.Println("Первая цифра", n / 100)
-	fmt.Println("Вторая цифра", (n / 10) % 10)
-	fmt.Println("Третья цифра", n % 10)
+	fmt.Println("Первая цифра", n/100)
+	fmt.Println("Вторая цифра", n/10%10)
+	fmt.Println("Третья цифра", n%10)
 }
