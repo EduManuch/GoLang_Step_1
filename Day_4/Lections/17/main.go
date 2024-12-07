ab2e58269dcb275c65a17b09756cf5cedeb15b15
Eduard <test@mail.ru>
2024-12-07 13:30:23 +0300

    Lection 17

---
+++ Day_4/Lections/17/main.go
@@ -0,0 +1,107 @@
+package main
+
+import "fmt"
+
+// 1. Структура - заименованный набор полей(состояний), определяющий новый тип данных
+
+// 2. Определение структуры(явное определение)
+type Student struct {
+	firstName string
+	lastName string
+	age int
+}
+
+
+type AnotherStudent struct {
+	firstName, lastName, groupName string
+	age, courseNumber int
+}
+
+type Human struct {
+	firstName string
+	lastName string
+	string
+	int
+	bool
+}
+
+func PrintStudent(std Student) {
+	fmt.Println("=======================")
+	fmt.Println("FirstName:", std.firstName)
+	fmt.Println("LastName:", std.lastName)
+	fmt.Println("Age:", std.age)
+}
+
+func main() {
+	student := Student{
+		firstName: "Fedya",
+		age: 21,
+		lastName: "Petrov",
+	}
+	PrintStudent(student)
+
+	student2 := Student{"Petr", "Ivanov", 19} // порядок указания свойств - такой же как и в стр-ре
+	PrintStudent(student2)
+
+	// 5. Что если не все поля структуры определены?
+	student3 := Student{
+		firstName: "Vasya",
+	}
+	PrintStudent(student3)
+
+	// 6. Анонимные структуры (структуры без имени)
+	anonStudent := struct{
+		age int
+		groupID int
+		professor string
+	}{
+		age: 23,
+		groupID: 2,
+		professor: "Alekseev",
+	}
+	fmt.Println("AnonStudent:", anonStudent)
+
+	// 7. Доступ к полям(состояниям) и их модификация// 7. Доступ к полям(состояниям) и их модификация
+	studMark := Student{"Mark", "Ivanov", 19}
+	fmt.Println("FirstName:", studMark.firstName)
+	fmt.Println("LastName:", studMark.lastName)
+	fmt.Println("Age:", studMark.age)
+	studMark.age += 2
+	fmt.Println("New age:", studMark.age)
+
+	// 8. Инициализация пустой структуры
+	emptyStudent1 := Student{}
+	var emptyStudent2 Student
+	PrintStudent(emptyStudent1)
+	PrintStudent(emptyStudent2)
+
+	// 9. Указатели на экземпляры структур
+	studPointer := &Student{
+		firstName: "Igor",
+		lastName: "Sidorov",
+		age: 23,
+	}
+	fmt.Println("Value of studPointer:", studPointer)
+
+	secondPointer := studPointer
+	(*secondPointer).age += 20
+	fmt.Println("Value of studPointer after changing:", studPointer)
+	studPointerNew := new(Student)
+	fmt.Println(studPointerNew)
+
+	// 10. Работа с доступом к полям через указатель
+	fmt.Println("Age via (*...).age:", (*studPointer).age)
+	// Неявно происходит разыменование и обращение к соответствующему полю
+	fmt.Println("Age via .age:", studPointer.age)
+
+	// 12. Создание экземпляра с анонимными полями
+	human := &Human{
+		firstName: "Bob",
+		lastName: "Smirnoff",
+		string: "Additional info",
+		int: -1,
+		bool: true,
+	}
+	fmt.Println(human)
+	fmt.Println("Anon field string:", human.string)
+}
